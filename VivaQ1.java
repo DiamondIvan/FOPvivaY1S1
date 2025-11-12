1dbbb9b1979b7d97d2a50b00d71e342be64ea9a1
IvanFong <fongjuntoh@gmail.com>
2025-11-12 23:30:57 +0800

    first commit

--- VivaQ1.java
+++ VivaQ1.java
@@ -6,6 +6,7 @@ public class VivaQ1 {
     public static void main(String[] args) {
         int numcase;
         int daysoverdue;
+        char booktype;git 
         Scanner sc = new Scanner(System.in);
         numcase = sc.nextInt();
         daysoverdue = sc.nextInt();
