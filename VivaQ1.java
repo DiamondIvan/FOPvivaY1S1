1994435ef0b8f7865cf28c6c2dca43a11f23a735
IvanFong <fongjuntoh@gmail.com>
2025-11-12 23:27:00 +0800

    first commit

---
+++ VivaQ1.java
@@ -0,0 +1,14 @@
+package FOPvivaY1S1;
+
+import java.util.Scanner;
+
+public class VivaQ1 {
+    public static void main(String[] args) {
+        int numcase;
+        int daysoverdue;
+        Scanner sc = new Scanner(System.in);
+        numcase = sc.nextInt();
+        daysoverdue = sc.nextInt();
+
+    }
+}
